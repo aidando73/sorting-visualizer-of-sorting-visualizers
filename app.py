167cb1abdac218701bbfd8de6ad38990b55f3ceb
Aidan Do <aidand@semsci.com>
2023-09-11 20:28:28 +0930

    continue

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from time import sleep
 from github import Github
 from github import Auth
 import os
+import json
 
 github_access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
 
@@ -9,14 +10,21 @@ auth = Auth.Token(github_access_token)
 
 g = Github(auth=auth)
 
+black_list = [
+    "klonnet23/helloy-word"
+]
+
 repositories = g.search_repositories(query='Sorting Visualizer', sort='stars', order='desc')
 print ("[")
 count = 0
 for repo in repositories:
+    if repo.full_name in black_list:
+        continue
     count += 1
-    print(f'{{"repoFullName": "{repo.full_name}", "stargazersCount": {repo.stargazers_count}, "ownerAvatarUrl": "{repo.owner.avatar_url}"}},')
-    if count % 900 == 0:
+    print(f'{{"repoFullName": "{repo.full_name}", "stargazersCount": {repo.stargazers_count}, "ownerAvatarUrl": "{repo.owner.avatar_url}", "language": "{repo.language}", "description": {json.dumps(repo.description)}, "forks": {repo.forks_count}, "updatedAt": "{repo.updated_at}"}},')
+    if count % 100 == 0:
         # Github API has a rate limit of 30 search requests per minute
         # there are 30 repositories per page
-        sleep(60)
+        # sleep(60)
+        break
 print ("]")
