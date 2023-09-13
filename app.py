c08252570024f352ac8f64d5417140f8b18f6d87
Aidan Do <aidand@semsci.com>
2023-09-13 17:24:07 +0930

    gogo

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ for repo in repositories:
         continue
     count += 1
     print(f'{{"repoFullName": "{repo.full_name}", "stargazersCount": {repo.stargazers_count}, "ownerAvatarUrl": "{repo.owner.avatar_url}", "language": "{repo.language}", "description": {json.dumps(repo.description)}, "forks": {repo.forks_count}, "updatedAt": "{repo.updated_at}"}},')
-    if count % 100 == 0:
+    if count == 200:
         # Github API has a rate limit of 30 search requests per minute
         # there are 30 repositories per page
         # sleep(60)
