3e6ade536c3bd3f1763c56e691e3bd144ae6b744
Aidan Do <aidand@semsci.com>
2023-09-26 19:42:15 +0930

    Added in sound with mute button

---
+++ beep.js
@@ -0,0 +1,68 @@
+var audioCtx = new (window.AudioContext || window.webkitAudioContext || window.audioContext);
+const DEFAULT_VOLUME = 0.5;
+
+function beepByStarCount(starCount) {
+   const rank = Math.log(starCount - minStarCount) / Math.log(maxStarCount - minStarCount);
+   beep(rank);
+}
+
+const primaryGainControl = audioCtx.createGain();
+primaryGainControl.gain.setValueAtTime(DEFAULT_VOLUME, audioCtx.currentTime);
+primaryGainControl.connect(audioCtx.destination);
+
+const highPassFilter = audioCtx.createBiquadFilter();
+highPassFilter.type = "highpass";
+highPassFilter.frequency.setValueAtTime(1200, audioCtx.currentTime);
+highPassFilter.Q.setValueAtTime(1, audioCtx.currentTime);
+highPassFilter.connect(primaryGainControl);
+
+const lowPassFilter = audioCtx.createBiquadFilter();
+lowPassFilter.type = "lowpass";
+lowPassFilter.frequency.setValueAtTime(300, audioCtx.currentTime);
+lowPassFilter.Q.setValueAtTime(1, audioCtx.currentTime);
+lowPassFilter.connect(highPassFilter);
+
+/**
+ * Rank is a number from 0 to 1
+ * 0 is the lowest pitch
+ * 1 is the highest pitch
+ */
+function beep(rank) {
+   rank = Math.max(0, Math.min(1, rank));
+   var oscillator = audioCtx.createOscillator();
+   var gainNode = audioCtx.createGain();
+   
+   
+   const volume = 0.50;
+   const duration = 0.025
+
+   const attack = 1/5*duration;
+   const decay = 2/5*duration;
+   const sustain = 0.5*volume;
+   const release = 0.1*duration;
+
+   oscillator.connect(gainNode);
+   gainNode.connect(lowPassFilter);
+
+   gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+   gainNode.gain.linearRampToValueAtTime(volume, audioCtx.currentTime + attack);
+   gainNode.gain.linearRampToValueAtTime(sustain, audioCtx.currentTime + attack + decay);
+   gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + attack + decay + release);
+
+   oscillator.type = "sine";
+   oscillator.frequency.value = 600 + 300 * rank;
+   oscillator.start();
+   oscillator.stop(audioCtx.currentTime + attack + decay + release);
+}
+
+function isMuted() {
+   return primaryGainControl.gain.value == 0;
+}
+
+function muteBeeps() {
+   primaryGainControl.gain.setValueAtTime(0, audioCtx.currentTime);
+}
+
+function unmuteBeeps() {
+   primaryGainControl.gain.setValueAtTime(DEFAULT_VOLUME, audioCtx.currentTime);
+}
